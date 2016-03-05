cebebab7b583004aaeb52b12fb5aa6eedfda0475
anderbre <anderbre@oregonstate.edu>
2016-03-05 08:26:01 -0700

    tab/spacing adjustments

--- add_up_athlete.php
+++ add_up_athlete.php
@@ -24,48 +24,42 @@ if($mysqli->connect_errno){
     $lname = $_POST['last_name'];
     $age = $_POST['age'];
     $teamName= $_POST['teamID'];
- 
+
 if(isset($_POST['Add']))
 {
-    
-if(!$mysqli || $mysqli->connect_errno){
-  echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+  if(!$mysqli || $mysqli->connect_errno){
+    echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
   }
 
-if(!($stmt = $mysqli->prepare("INSERT INTO `athletes` (`first_name`,`last_name`,`age`,`teamID`) VALUES (?,?,?,?)"))){
-  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-}
-if(!($stmt->bind_param("ssii",$fname , $lname, $age, $teamName))){
-  echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
-}
-if(!$stmt->execute()){
-  echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
-} else {
-  echo "Athlete has been added to database.";}
-}
-
-else
+  if(!($stmt = $mysqli->prepare("INSERT INTO `athletes` (`first_name`,`last_name`,`age`,`teamID`) VALUES (?,?,?,?)"))){
+    echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+  }
+  if(!($stmt->bind_param("ssii",$fname , $lname, $age, $teamName))){
+    echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
+  }
+  if(!$stmt->execute()){
+    echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+  } else {
+    echo "Athlete has been added to database.";}
+} else
 {
-
-if(!($updateQ = $mysqli->prepare("UPDATE athletes SET teamID=?, age=? WHERE first_name=?  AND last_name=? "))){
-  echo "Prepare failed: "  . $updateQ->errno . " " . $updateQ->error;
-}
-if(!($updateQ->bind_param("iiss",$teamName , $age, $fname, $lname))){
-  echo "Bind failed: "  . $updateQ->errno . " " . $updateQ->error;
-}
-if(!$updateQ->execute()){
-  echo "Execute failed: "  . $updateQ->errno . " " . $updateQ->error;
-} 
-else if($updateQ->affected_rows > 0)
-{
-  echo "Athlete age and team have been updated!";
-}
+  if(!($updateQ = $mysqli->prepare("UPDATE athletes SET teamID=?, age=? WHERE first_name=?  AND last_name=? "))){
+    echo "Prepare failed: "  . $updateQ->errno . " " . $updateQ->error;
+  }
+  if(!($updateQ->bind_param("iiss",$teamName , $age, $fname, $lname))){
+    echo "Bind failed: "  . $updateQ->errno . " " . $updateQ->error;
+  }
+  if(!$updateQ->execute()){
+    echo "Execute failed: "  . $updateQ->errno . " " . $updateQ->error;
+  }
+  else if($updateQ->affected_rows > 0)
+  {
+    echo "Athlete age and team have been updated!";
+  }
   else
   {
     echo "No rows affected, please enter the first name and last of current athlete to update.";
   }
-
-
 }
 
 
